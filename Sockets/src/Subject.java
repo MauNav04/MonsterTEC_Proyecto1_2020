541a19f8e7ff68ae8b79362d35dcc20bebbb8484
mauro <mauronav006@gmail.com>
2020-10-29 00:39:22 -0600

    [ADD] Funcionalidades a Subject
    Se ordenan un poco los métodos de la clase Subject y se le agregan nuevas funcionalidades para que se pueda conectar a un servidor cuando se inicia

--- Sockets/src/Subject.java
+++ Sockets/src/Subject.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
+import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.Random;
 import java.util.Scanner;
 
 //informacion a recibir (bool-)
@@ -7,16 +9,21 @@ import java.util.Scanner;
 public class Subject {
     //private boolean palying = false;
     private RecieverSocket listener;
-    private SendingSocket dealer;
+    private SendingSocket deliver;
+    private boolean connected = false;
     public boolean actionControl = false;// false recibir - true enviar
     public int life = 0;
     public int mana = 0;
     public LinkedLists hand;
     public LinkedLists Deck;
+    public int ServersConnectionPort=2637;
+    private String subjectPort;
+    private String subjectIP;
 
-    public Subject() {
-        ActionPerformer();
+    public Subject() throws UnknownHostException {
+        initializer();
 
+        //ActionPerformer();
         /*if(actionControl){
             int port = PortToContact();
             String message = MessageCreator();
@@ -29,19 +36,13 @@ public class Subject {
     }
 
 
+    //Primero se debe crear el listener para poder pasar el puerto como parametro
 
-    private int PortToContact(){
-        System.out.println("Select a port to send: ");
-        Scanner scanner = new Scanner(System.in);
-        int port = scanner.nextInt();
-        return port;
-    }
-
-    private String MessageCreator(){
-        System.out.println("Type a message: ");
-        Scanner scanner = new Scanner(System.in);
-        String message = scanner.nextLine();
-        return message;
+    private void initializer() throws UnknownHostException {
+        HostSubject();
+        String message = MessageCreator(".newPlayer-"+this.subjectPort);
+        int connectToPort = PortToContact(this.ServersConnectionPort);
+        SEND(message,connectToPort);
     }
 
     // 0 para recibir mensaje - 1 para enviar mensaje
@@ -50,18 +51,39 @@ public class Subject {
         Scanner scanner = new Scanner(System.in);
         int action = scanner.nextInt();
         if(action == 1) ActionChanger();
-    }*/
+    }
 
     private void ActionPerformer(){
         if(this.getClass().getCanonicalName() == "Subject"){
-            listenAction();
+            LISTEN(Integer.parseInt(this.subjectPort));
         }
+    }*/
+
+
+    //Se cambiará por un set a la variable
+    private int PortToContact(int port){
+        return port;
     }
 
-    private void listenAction (){
-        int port = PortSelector();
+    //Codificará el mensaje en un JSON
+    private String MessageCreator(String message){
+        return message;
+    }
+
+    private void SEND(String message, int portToConnect) {
+        deliver = new SendingSocket("", portToConnect, message);
+        System.out.println("Message Sent successfully");
+    }
+
+    private void LISTEN(int port){
         listener = new RecieverSocket(port);
-        InfoUpdate(listener.getInfo());
+        String message = listener.getInfo();
+        //InfoProcessor(message);
+    }
+
+    private void HostSubject() throws UnknownHostException {
+        getIp();
+        getPort();
     }
 
     private void InfoUpdate(String message){ //message structure "50,80" - "Life-Mana" (order)
@@ -74,18 +96,28 @@ public class Subject {
         System.out.println(mana);
     }
 
-    private int PortSelector(){
-        System.out.println("Insert a port to host: ");
-        Scanner scanner = new Scanner(System.in);
-        int port = scanner.nextInt();
-        return port;
+    private void getIp() throws UnknownHostException {
+        InetAddress ip = InetAddress.getLocalHost();
+        String strIp = ip.toString();
+        String[] splitStrIp = strIp.split("/");
+        String ipNumber = splitStrIp[1];
+        System.out.println("Current IP: "+ ipNumber);
+        this.subjectIP = ipNumber;
+    }
+
+    private void getPort(){
+        Random randomInstance = new Random();
+        Integer randomPort = randomInstance.nextInt((4000 - 2000) + 1) + 2000;
+        String strPort = randomPort.toString();
+        System.out.println("Current Port: "+ strPort);
+        this.subjectPort = strPort;
     }
 
     private void ActionChanger (){
         this.actionControl = !actionControl;
     }
 
-    public static void main(String args[]){
+    public static void main(String args[]) throws UnknownHostException {
         Subject talker = new Subject();
     }
 
