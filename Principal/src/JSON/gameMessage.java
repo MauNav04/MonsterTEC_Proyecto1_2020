30ee5b3a3b084426f377b66616afa827622759b1
mauro <mauronav006@gmail.com>
2020-11-10 05:23:31 -0600

    [ADD] Client answers to server
    Se añaden algunas respuestas del cliente para cuando el servidor da el permiso de jugar

---
+++ Principal/src/JSON/gameMessage.java
@@ -0,0 +1,30 @@
+package JSON;
+
+import GameObjects.PlayingCard;
+
+public class gameMessage extends Message {
+
+    //Actualizacion de SERVER - CLIENTE
+    public gameMessage(String action, Boolean Deck, int amountDeckCards, PlayingCard topDeckCard, int playerLife, int playerMana) {
+        this.action = action;
+        this.Deck = Deck;
+        this.amountDeckCards = amountDeckCards;
+        this.topDeckCard = topDeckCard;
+        this.playerLife = playerLife;
+        this.playerMana = playerMana;
+    }
+
+    // Se salta - o se saca de baraja
+    public gameMessage(String action, Boolean end){
+        this.action = action;
+        this.end = end;
+    }
+
+    //Se elige jugar carta
+    public gameMessage(String action, PlayingCard card, Boolean end){
+        this.action = action;
+        this.card = card;
+        this.end = end;
+    }
+
+}
